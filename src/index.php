3a4dff5945ebbeb0868179e60a3d3a54e8261892
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-09-10 09:49:14 -0500

    Remove embedded git repositories and update .gitignore

--- src/index.php
+++ src/index.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "Hola, mundo!";
+require 'vendor/autoload.php';
+
+use MongoDB\Client;
+
+try {
+    $client = new Client("mongodb://mongo:27017");
+
+    $collection = $client->test->users;
+
+    $insertResult = $collection->insertOne(['name' => 'John Doe', 'email' => 'john.doe@example.com']);
+
+    echo "Inserted with Object ID '{$insertResult->getInsertedId()}'";
+
+    $user = $collection->findOne(['name' => 'John Doe']);
+    if ($user) {
+        echo "Found user: " . $user['name'] . " with email: " . $user['email'];
+    } else {
+        echo "User not found";
+    }
+} catch (Exception $e) {
+    echo "An error occurred: ";
+}
+
+?>
