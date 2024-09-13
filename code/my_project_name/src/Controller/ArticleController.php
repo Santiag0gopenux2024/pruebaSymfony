8896866282444f829fb4923ea894d79fb6f160e7
Santiag0gopenux2024 <michael.martinez@gopenux.com>
2024-09-13 07:30:11 -0500

    Descripción de los cambios

---
+++ code/my_project_name/src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    public function index($_locale, $year, $slug, $_format): Response
+    {
+        $message = "language Selected: ".$_locale.", year selected: ".$year.", slug selected: ".$slug.", format selected: ".$_format;
+        return $this->json( [
+            'message' => $message,
+            'path' => 'src/Controller/ArticleController.php',
+        ]);
+    }
+}
